066a2201a0443acf2fda96340dbc1c148370213c
minhtetnaing117 <59909903+minhtetnaing117@users.noreply.github.com>
2024-06-15 15:37:39 +0630

    logo and title initial commit

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -6,6 +6,52 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(height: 50),
+
+              // logo
+              const Icon(
+                Icons.lock,
+                size: 100,
+              ),
+
+
+              const SizedBox(height: 50),
+
+              // welcome back, you've been missed!
+              Text(
+                  "Welcome back you\'ve been missed!",
+                style: TextStyle(
+                    color: Colors.grey[700],
+                  fontSize: 16,
+                ),
+              ),
+
+              const SizedBox(height: 50),
+
+              // username
+
+              // pass
+
+              // forgot pass
+
+              //sign in
+
+              // or continue
+
+              // google + apple sign in
+
+              // not a member? register now
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
